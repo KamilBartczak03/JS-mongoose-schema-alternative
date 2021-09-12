b8151ac17a2d5f41be3df72864fcae68815b9657
KamilBartczak03 <89569524+KamilBartczak03@users.noreply.github.com>
2021-09-12 21:00:54 +0200

    Required function fix

--- module.js
+++ module.js
@@ -1,50 +1,60 @@
-export function $required(boolean, message){
-    return {
-        value: boolean,
-        error: message,
-        handler(currentKey) {
-            if(this.value === true && currentKey === null || currentKey === undefined || currentKey === "") throw this.error
-        }
-    }
+function getType(value) {
+  let type = Object.prototype.toString.call(value)
+  type = type.slice(8, Infinity)
+  return type.slice(0, type.length - 1)
 }
 
-export function $type(type, message){
-    return {
-        value: type,
-        error: message,
-        handler(currentKey) {
-            if(typeof currentKey !== this.value) throw this.error
-        }
+export function $required(boolean, message) {
+  return {
+    value: boolean,
+    error: message,
+    handler(currentKey) {
+      if (this.value === false) return
+      if (currentKey === null || currentKey === undefined || currentKey === "") throw this.error
     }
+  }
 }
 
-export function $int(boolean, message){
-    return {
-        value: boolean,
-        error: message,
-        handler(currentKey) {
-            if(this.value && currentKey % 1 !== 0) throw this.error
-        }
+export function $type(type, message) {
+  return {
+    value: type,
+    error: message,
+    handler(currentKey) {
+      if (getType(currentKey) !== this.value) throw this.error
     }
+  }
 }
 
-export class Schema{
-    constructor(schema, collection_name){
-        this.schema = schema
+export function $int(boolean, message) {
+  return {
+    value: boolean,
+    error: message,
+    handler(currentKey) {
+      if (this.value && currentKey % 1 !== 0) throw this.error
     }
+  }
+}
 
-    async validate(payload){        
-        for (const key in this.schema) {
-            let errorMessage = null
-            const currentSchema = this.schema[key]
-            const currentKey = payload[key]
-            
-            if(currentSchema.required ) currentSchema.required.handler(currentKey, currentSchema)
-            if(currentSchema.type) currentSchema.type.handler(currentKey, currentSchema)
-            if(currentSchema.schema) await currentSchema.schema.validate(currentKey).catch(error => errorMessage =  error)
-            if(currentSchema.int) currentSchema.int.handler(currentKey, currentSchema)
+export class Schema {
+  constructor(schema, collection_name) {
+    this.schema = schema
+  }
 
-            if(errorMessage) throw errorMessage
-        }
+  async validate(payload) {
+    for (const key in this.schema) {
+      let errorMessage = null
+      const currentSchema = this.schema[key]
+      const currentKey = payload[key]
+
+      if (currentSchema.required) currentSchema.required.handler(currentKey, currentSchema)
+        if (currentKey !== null && currentKey !== undefined && currentKey !== ""){
+        if (currentSchema.type) currentSchema.type.handler(currentKey, currentSchema)
+        if (currentSchema.schema) await currentSchema.schema.validate(currentKey).catch(error => errorMessage = error)
+        if (currentSchema.int) currentSchema.int.handler(currentKey, currentSchema)
+        if (currentSchema.validator) await currentSchema.validator(currentKey).catch(error => errorMessage = error)
+      }
+      
+      if (errorMessage) throw errorMessage
     }
+  }
 }
